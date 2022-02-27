8097734f755a510ce8e8e658b4cab9328c94c42a
David DaSilva <daviddasilvajr@gmail.com>
2022-02-27 16:26:57 -0500

    sql for getting pics from folder

--- greatjobdave/site.py
+++ greatjobdave/site.py
@@ -1,25 +1,41 @@
+import logging
+
 from flask import render_template, Blueprint
+from greatjobdave.db import get_db
 
 bp = Blueprint('site', __name__)
 
+
 @bp.route('/')
 def index():
-    data = 'greatjobdave/static/assets/pics/pic22.jpeg'
+    db = get_db()
+    data = db.execute('SELECT p.id, p.photo_folder_id, p.name FROM photos p ORDER BY name ASC').fetchall()
     return render_template('site/index.html', photo=data)
 
 
 @bp.route('/projects')
 def projects():
-    return render_template('site/projects.html', photo=data)
+    db = get_db()
+    links = db.execute('SELECT text, url, embedded FROM links ORDER BY id ASC').fetchall()
+    return render_template('site/projects.html', data=links)
 
 
 @bp.route('/notes')
 def notes():
-    data = 'greatjobdave/static/assets/pics/pic22.jpeg'
-    return render_template('/site/notes.html', photo=data)
+    db = get_db()
+    data=db.execute('SELECT n.title, n.body, n.date FROM notes n ORDER BY date ASC').fetchall()
+    return render_template('/site/notes.html', notes=data)
 
 
-@bp.route('/photos')
-def photos():
-    data = 'greatjobdave/static/assets/pics/pic22.jpeg'
-    return render_template('/site/photos.html', photo=data)
+@bp.route('/photos/<folder>')
+def photos(folder):
+    db = get_db()
+    list = db.execute("SELECT name FROM photo_folder")
+    data = db.execute(
+        f"SELECT f.path, photos.name " \
+        f"FROM photos " \
+        f"LEFT JOIN photo_folder f " \
+        f"on photos.photo_folder_id = f.id " \
+        f"WHERE f.name = {folder}")
+
+    return render_template('/site/photos.html', data=data, list=list)
